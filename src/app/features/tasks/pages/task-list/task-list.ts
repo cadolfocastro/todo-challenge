2abf7b864530ffe79eb98ea81cc5712c60bbaf32
cadolfocastro <cadolfocastro.ing@gmail.com>
2026-03-12 23:19:49 -0500

    feat: enhance edit task modal with validation and styling improvements

--- src/app/features/tasks/pages/task-list/task-list.ts
+++ src/app/features/tasks/pages/task-list/task-list.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core'
-
 import { FormsModule } from '@angular/forms'
 import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop'
 import { Task } from '../../models/task'
