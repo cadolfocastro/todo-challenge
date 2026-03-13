98b3c0e995effd7c6197a541d9d5f7dfc042e164
cadolfocastro <cadolfocastro.ing@gmail.com>
2026-03-13 01:46:06 -0500

    feat: update task column styles for improved layout and responsiveness

--- src/app/features/tasks/components/task-column/task-column.ts
+++ src/app/features/tasks/components/task-column/task-column.ts
@@ -1,13 +1,13 @@
 import { Component, Input } from '@angular/core'
-
 import { DragDropModule } from '@angular/cdk/drag-drop'
 import { Task } from '../../models/task'
 import { TaskCard } from '../task-card/task-card'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-task-column',
   standalone: true,
-  imports: [DragDropModule, TaskCard],
+  imports: [DragDropModule, TaskCard, CommonModule],
   templateUrl: './task-column.html',
   styleUrl: './task-column.css'
 })
